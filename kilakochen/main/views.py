5f1bcacb1b6f7be3dab492a6ec200c9d89a43f02
Christian Hasenburg <christian.hasenburg@kinderladen-jakobistrasse.de>
2024-07-08 13:57:56 +0200

    refactoring vom datenbank model

--- kilakochen/main/views.py
+++ kilakochen/main/views.py
@@ -1,8 +1,8 @@
 from kilakochen.main import bp    
 from flask import render_template
-
+from flask_weasyprint import HTML, render_pdf
 from datetime import datetime
-from kilakochen.models import Essensplan, Rezepte, Zutaten,ZutatenProRezept,Mengenangabe, Zutatengruppe
+from kilakochen.models import Allergene, Essensplan, Rezepte, Zutaten
 
 from kilakochen import db
 
@@ -13,17 +13,9 @@ def index():
 @bp.route('/heute')
 def heute():
     date = datetime.now()
+    data = Essensplan.query.filter_by(
+        Datum=date.date()).one_or_404()
 
-    res = Essensplan.query.filter_by(
-        wplan_kw=date.isocalendar().week,
-        wplan_wtag=date.isocalendar().weekday,
-        wplan_jahr=date.isocalendar().year
-        ).one_or_404()
-    
-    hgericht = Rezepte.query.filter_by(rezept_id=res.wplan_hgericht).first().rezept_name
-    beilage =  Rezepte.query.filter_by(rezept_id=res.wplan_beilage).first().rezept_name
-    dessert =  Rezepte.query.filter_by(rezept_id=res.wplan_dessert).first().rezept_name
-    data = [hgericht, beilage, dessert]    
     return render_template(
         'heute.html',
         date = date,
@@ -31,47 +23,52 @@ def heute():
         data=data
     )
 
-@bp.route('/rezepte/<int:id>')
-@bp.route('/rezepte', defaults={'id' : 0})
-def rezepte(id):
-    if id > 0:
-        data = {}
-        data["rezept"] = Rezepte.query.filter_by(rezept_id=id).first()
-        rezept_name = data["rezept"].rezept_name
-        res_zutaten = ZutatenProRezept.query.filter_by(Rezept_ID=id).all()
-        data["zutaten_header"] = ["Menge", "Einheit", "Zutat"]
-        data["zutaten"] = []
-        for zutat in res_zutaten:
-            zutat_data = []
-            tmp = Zutaten.query.filter_by(zutat_id=zutat.Zutat_ID).one()
-            mengen_einheit = Mengenangabe.query.filter_by(angaben_id=zutat.Einheit).one()
-            zutat_data.append(zutat.Menge)
-            zutat_data.append(mengen_einheit.angaben_abkz)
-            zutat_data.append(tmp.zutat_name)
-            data["zutaten"].append(zutat_data)
-        return render_template(
-            'rezept.html',
-            page_title = "Rezept - " + rezept_name,
-            rezept_name = rezept_name,
-            data=data
-        )
-    else:
-        data = Rezepte.query.order_by(Rezepte.rezept_name).all()
-        return render_template(
-            'rezepte.html',
-            page_title = "Übersicht der Rezepte",
-            data=data
-        )
+
+@bp.route('/rezepte')
+def rezepte():
+    data = Rezepte.query.order_by(Rezepte.Titel).all()
+    return render_template(
+        'rezepte.html',
+        page_title = "Übersicht der Rezepte",
+        data=data
+    )
+
+@bp.route('/rezept/<int:id>')
+def rezept(id):
+    data = Rezepte.query.filter_by(ID=id).one_or_404()
     
+    return render_template(
+        'rezept.html',
+        page_title = "Rezept - " + data.Titel,
+        rezept_name = data.Titel,
+        data=data
+    )
+
+@bp.route('/rezept/print/<int:id>')
+def rezepte(id):    
+    data = Rezepte.query.filter_by(ID=id).one_or_404()
+    
+    html = render_template(
+        'print_base.html',
+        page_title = "Rezept - " + data.Titel,
+        rezept_name = data.Titel,
+        data=data
+    )
+
+    return render_pdf(HTML(string=html))
+
+
+
 
 @bp.route('/zutaten')
 def zutaten():
-    data = Zutaten.query.all()
-#    data = Zutaten.query.order_by("zutat_name").all()
+    zutaten = Zutaten.query.all()
+    allergene = Allergene.query.all()
     return render_template(
         'zutaten.html',
         page_title = "Übersicht aller Zutaten",
-        data = data
+        data = zutaten,
+        allergene = allergene
     )
 
 @bp.route('/wochenplan')
--- kilakochen/models.py
+++ kilakochen/models.py
@@ -6,7 +6,14 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 import datetime
 import decimal
 
-from flask_sqlalchemy import SQLAlchemy
+from typing import Any, List, Optional
+
+from sqlalchemy import CHAR, Date, Double, ForeignKeyConstraint, Index, String, TIMESTAMP, Text, text
+from sqlalchemy.dialects.mysql import BIT, DECIMAL, INTEGER, LONGTEXT, MEDIUMTEXT, TINYINT
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import datetime
+import decimal
+
 from flask_login import UserMixin
 
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -21,7 +28,7 @@ def load_user(id):
 
    
 class User(db.Model, UserMixin):
-    __tablename__ = 'kila_jakobi_anwender'
+    __tablename__ = 'anwender'
 
     ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
     name: Mapped[str] = mapped_column(Text)
@@ -39,107 +46,221 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password,password)
 
 
-class Essensplan(db.Model):
-    __tablename__ = 'kila_jakobi_essensplan'
-
-    wplan_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    wplan_kw: Mapped[int] = mapped_column(INTEGER(11))
-    wplan_jahr: Mapped[int] = mapped_column(INTEGER(11))
-    wplan_wtag: Mapped[int] = mapped_column(INTEGER(11))
-    wplan_hgericht: Mapped[Optional[int]] = mapped_column(INTEGER(11))
-    wplan_beilage: Mapped[Optional[int]] = mapped_column(INTEGER(11))
-    wplan_dessert: Mapped[Optional[int]] = mapped_column(INTEGER(11))
-    wplan_ausfall: Mapped[Optional[str]] = mapped_column(Text)
-    wplan_note: Mapped[Optional[str]] = mapped_column(Text)
-
-
-class Mengenangabe(db.Model):
-    __tablename__ = 'kila_jakobi_mengenangabe'
+class Allergene(db.Model):
+    __tablename__ = 'allergene'
     __table_args__ = (
-        Index('angaben_name', 'angaben_name', unique=True),
-        Index('angaben_name_2', 'angaben_name')
+        Index('Bezeichnung', 'Bezeichnung', unique=True),
+        Index('Kuerzel', 'Kuerzel', unique=True)
     )
 
-    angaben_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    angaben_name: Mapped[str] = mapped_column(TEXT)
-    angaben_abkz: Mapped[str] = mapped_column(TEXT)
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    Kuerzel: Mapped[str] = mapped_column(CHAR(2))
+    Bezeichnung: Mapped[str] = mapped_column(String(50))
+    Aktiv: Mapped[int] = mapped_column(TINYINT(1), server_default=text('1'))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    Beschreibung: Mapped[Optional[str]] = mapped_column(Text)
+    Kommentar: Mapped[Optional[str]] = mapped_column(Text)
+
+    zutaten_allergene: Mapped[List['ZutatenAllergene']] = relationship('ZutatenAllergene', back_populates='allergene')
+
+
+class Einheiten(db.Model):
+    __tablename__ = 'einheiten'
+    __table_args__ = (
+        Index('Bezeichnung', 'Bezeichnung', unique=True),
+        Index('Kuerzel', 'Kuerzel', unique=True)
+    )
+
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    Kuerzel: Mapped[str] = mapped_column(CHAR(3))
+    Bezeichnung: Mapped[str] = mapped_column(String(50))
+    Beschreibung: Mapped[str] = mapped_column(MEDIUMTEXT)
+    Aktiv: Mapped[int] = mapped_column(TINYINT(1), server_default=text('1'))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    BasiseinheitID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    BasiseinheitFaktor: Mapped[Optional[decimal.Decimal]] = mapped_column(DECIMAL(16, 8))
+    Kommentar: Mapped[Optional[str]] = mapped_column(Text)
+    Sortierung: Mapped[Optional[int]] = mapped_column(TINYINT(2))
+
+    rezepte_zutaten: Mapped[List['RezepteZutaten']] = relationship('RezepteZutaten', back_populates='einheiten')
+
+
+class Rezeptkategorien(db.Model):
+    __tablename__ = 'rezeptkategorien'
+    __table_args__ = (
+        Index('BezeichnungPlural', 'BezeichnungPlural', unique=True),
+        Index('BezeichnungSingular', 'BezeichnungSingular', unique=True),
+        Index('Kuerzel', 'Kuerzel', unique=True),
+        Index('URL', 'URL', unique=True)
+    )
+
+    ID: Mapped[int] = mapped_column(INTEGER(10), primary_key=True)
+    BezeichnungSingular: Mapped[str] = mapped_column(String(50))
+    BezeichnungPlural: Mapped[str] = mapped_column(String(50))
+    URL: Mapped[str] = mapped_column(String(60))
+    Aktiv: Mapped[int] = mapped_column(TINYINT(1), server_default=text('1'))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    Ueberarbeitet: Mapped[Any] = mapped_column(BIT(1))
+    Beschreibung: Mapped[Optional[str]] = mapped_column(MEDIUMTEXT)
+    Kuerzel: Mapped[Optional[str]] = mapped_column(CHAR(3))
+    Sortierung: Mapped[Optional[int]] = mapped_column(TINYINT(2))
+
+    rezepte: Mapped[List['Rezepte']] = relationship('Rezepte', back_populates='rezeptkategorien')
+    speiseplan: Mapped[List['Speiseplan']] = relationship('Speiseplan', back_populates='rezeptkategorien')
+
+
+class Zutatengruppen(db.Model):
+    __tablename__ = 'zutatengruppen'
+    __table_args__ = (
+        Index('Bezeichnung', 'Bezeichnung', unique=True),
+        Index('Kuerzel', 'Kuerzel', unique=True)
+    )
+
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    Bezeichnung: Mapped[str] = mapped_column(String(50))
+    Aktiv: Mapped[int] = mapped_column(TINYINT(1), server_default=text('1'))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    Ueberarbeitet: Mapped[Any] = mapped_column(BIT(1))
+    Kuerzel: Mapped[Optional[str]] = mapped_column(CHAR(3))
+    Sortierung: Mapped[Optional[int]] = mapped_column(TINYINT(2))
+
+    zutaten: Mapped[List['Zutaten']] = relationship('Zutaten', back_populates='zutatengruppen')
 
 
 class Rezepte(db.Model):
-    __tablename__ = 'kila_jakobi_rezepte'
+    __tablename__ = 'rezepte'
     __table_args__ = (
-        Index('rezept_name', 'rezept_name', unique=True),
+        ForeignKeyConstraint(['KategorieID'], ['rezeptkategorien.ID'], name='rezepte_rezeptkategorien'),
+        Index('Titel', 'Titel', unique=True),
+        Index('rezepte_rezeptkategorien', 'KategorieID')
     )
 
-    rezept_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    rezept_name: Mapped[str] = mapped_column(TEXT)
-    rezept_desc: Mapped[str] = mapped_column(TEXT)
-    rezept_cat: Mapped[str] = mapped_column(TEXT)
-    hgericht: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    beilage: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    dessert: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    rezept_auth: Mapped[str] = mapped_column(TEXT)
-    rezept_date: Mapped[datetime.date] = mapped_column(Date)
-    rezept_text: Mapped[str] = mapped_column(MEDIUMTEXT)
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    Titel: Mapped[str] = mapped_column(String(50))
+    Zubereitung: Mapped[str] = mapped_column(LONGTEXT)
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    Ueberarbeitet: Mapped[Any] = mapped_column(BIT(1))
+    author: Mapped[str] = mapped_column(MEDIUMTEXT)
+    created_at: Mapped[datetime.date] = mapped_column(Date)
+    KategorieID: Mapped[Optional[int]] = mapped_column(INTEGER(10))
+
+    rezeptkategorien: Mapped['Rezeptkategorien'] = relationship('Rezeptkategorien', back_populates='rezepte')
+    essensplan_beilage: Mapped[List['Essensplan']] = relationship('Essensplan', foreign_keys='[Essensplan.BeilageRezeptID]', back_populates='Beilage')
+    essensplan_dessert: Mapped[List['Essensplan']] = relationship('Essensplan', foreign_keys='[Essensplan.DessertRezeptID]', back_populates='Dessert')
+    essensplan_hauptgericht: Mapped[List['Essensplan']] = relationship('Essensplan', foreign_keys='[Essensplan.HauptgerichtRezeptID]', back_populates='Hauptgericht')
+    rezepte_zutaten: Mapped[List['RezepteZutaten']] = relationship('RezepteZutaten', back_populates='rezepte')
+    speiseplan: Mapped[List['Speiseplan']] = relationship('Speiseplan', back_populates='rezepte')
 
 
 class Zutaten(db.Model):
-    __tablename__ = 'kila_jakobi_zutaten'
+    __tablename__ = 'zutaten'
     __table_args__ = (
-        Index('I_Z_NAME', 'zutat_name', unique=True),
+        ForeignKeyConstraint(['GruppeID'], ['zutatengruppen.ID'], name='zutaten_zutatengruppen'),
+        Index('Bezeichnung', 'Bezeichnung', unique=True),
+        Index('zutaten_zutatengruppen_ID', 'GruppeID')
     )
 
-    zutat_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-
-    allergen_ei: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_en: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_fi: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_gl: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_kr: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_lu: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_mi: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_s: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_sw: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_sl: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_sf: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_se: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_so: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    allergen_wt: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_farbst: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_konserv: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_antioxi: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_geschmv: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_gschwefel: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_gschwarz: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_phosphat: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_milcheiw: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_koffein: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_chinin: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_suessung: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_phenylan: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_gwachs: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zusatz_taurin: Mapped[int] = mapped_column(INTEGER(11), server_default=text('0'))
-    zutat_name: Mapped[Optional[str]] = mapped_column(TEXT)
-    zutat_quelle: Mapped[Optional[str]] = mapped_column(Text)
-    zutat_auth: Mapped[Optional[str]] = mapped_column(Text)
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    Bezeichnung: Mapped[str] = mapped_column(String(50))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    Ueberarbeitet: Mapped[Any] = mapped_column(BIT(1))
+    Beschreibung: Mapped[Optional[str]] = mapped_column(MEDIUMTEXT)
+    GruppeID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    Aktiv: Mapped[Optional[int]] = mapped_column(TINYINT(1), server_default=text('1'))
+    Sortierung: Mapped[Optional[int]] = mapped_column(TINYINT(2))
+    zutat_quelle: Mapped[Optional[str]] = mapped_column(MEDIUMTEXT)
+    zutat_auth: Mapped[Optional[str]] = mapped_column(MEDIUMTEXT)
     zutat_date: Mapped[Optional[datetime.date]] = mapped_column(Date)
-    zutat_gruppe: Mapped[int] = mapped_column(ForeignKey("kila_jakobi_zutatengruppe.zutatengruppe_id"))
-    zutaten_gruppe: Mapped["Zutatengruppe"] = relationship("Zutatengruppe", back_populates="zutaten")
+
+    zutatengruppen: Mapped['Zutatengruppen'] = relationship('Zutatengruppen', back_populates='zutaten')
+    rezepte_zutaten: Mapped[List['RezepteZutaten']] = relationship('RezepteZutaten', back_populates='zutaten')
+    zutaten_allergene: Mapped[List['ZutatenAllergene']] = relationship('ZutatenAllergene', back_populates='zutaten')
 
 
-class ZutatenProRezept(db.Model):
-    __tablename__ = 'kila_jakobi_zutaten_pro_rezept'
+class Essensplan(db.Model):
+    __tablename__ = 'essensplan'
+    __table_args__ = (
+        ForeignKeyConstraint(['BeilageRezeptID'], ['rezepte.ID'], name='essensplan_rezepte_Beilage'),
+        ForeignKeyConstraint(['DessertRezeptID'], ['rezepte.ID'], name='essensplan_rezepte_Dessert'),
+        ForeignKeyConstraint(['HauptgerichtRezeptID'], ['rezepte.ID'], name='essensplan_rezepte_Hautgericht'),
+        Index('Datum', 'Datum', unique=True),
+        Index('essensplan_rezepte_Beilage', 'BeilageRezeptID'),
+        Index('essensplan_rezepte_Dessert', 'DessertRezeptID'),
+        Index('essensplan_rezepte_Hautgericht', 'HauptgerichtRezeptID')
+    )
 
     ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    Rezept_ID: Mapped[int] = mapped_column(INTEGER(11))
-    Zutat_ID: Mapped[int] = mapped_column(INTEGER(11))
-    Menge: Mapped[decimal.Decimal] = mapped_column(Double(asdecimal=False))
-    Einheit: Mapped[int] = mapped_column(INTEGER(2))
+    Datum: Mapped[datetime.date] = mapped_column(Date)
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    HauptgerichtRezeptID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    BeilageRezeptID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    DessertRezeptID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    Ausfall: Mapped[Optional[str]] = mapped_column(String(50))
+    Anmerkung: Mapped[Optional[str]] = mapped_column(String(400))
+
+    Beilage: Mapped['Rezepte'] = relationship('Rezepte', foreign_keys=[BeilageRezeptID], back_populates='essensplan_beilage')
+    Dessert: Mapped['Rezepte'] = relationship('Rezepte', foreign_keys=[DessertRezeptID], back_populates='essensplan_dessert')
+    Hauptgericht: Mapped['Rezepte'] = relationship('Rezepte', foreign_keys=[HauptgerichtRezeptID], back_populates='essensplan_hauptgericht')
 
 
-class Zutatengruppe(db.Model):
-    __tablename__ = 'kila_jakobi_zutatengruppe'
+class RezepteZutaten(db.Model):
+    __tablename__ = 'rezepte_zutaten'
+    __table_args__ = (
+        ForeignKeyConstraint(['EinheitID'], ['einheiten.ID'], onupdate='CASCADE', name='rezepte_zutaten_einheiten'),
+        ForeignKeyConstraint(['RezeptID'], ['rezepte.ID'], ondelete='CASCADE', onupdate='CASCADE', name='rezepte_zutaten_rezepte'),
+        ForeignKeyConstraint(['ZutatID'], ['zutaten.ID'], ondelete='CASCADE', onupdate='CASCADE', name='rezepte_zutaten_zutaten'),
+        Index('RezeptID_ZutatID', 'RezeptID', 'ZutatID', unique=True),
+        Index('rezepte_zutaten_einheiten', 'EinheitID'),
+        Index('rezepte_zutaten_zutaten', 'ZutatID')
+    )
 
-    zutatengruppe_id: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
-    zutatengruppe_name: Mapped[str] = mapped_column(Text)
-    zutaten: Mapped[list["Zutaten"]] = relationship("Zutaten", back_populates="zutaten_gruppe")
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    RezeptID: Mapped[int] = mapped_column(INTEGER(11))
+    ZutatID: Mapped[int] = mapped_column(INTEGER(11))
+    Menge: Mapped[decimal.Decimal] = mapped_column(Double(asdecimal=True))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    Ueberarbeitet: Mapped[Any] = mapped_column(BIT(1))
+    EinheitID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+
+    einheiten: Mapped['Einheiten'] = relationship('Einheiten', back_populates='rezepte_zutaten')
+    rezepte: Mapped['Rezepte'] = relationship('Rezepte', back_populates='rezepte_zutaten')
+    zutaten: Mapped['Zutaten'] = relationship('Zutaten', back_populates='rezepte_zutaten')
+
+
+class Speiseplan(db.Model):
+    __tablename__ = 'speiseplan'
+    __table_args__ = (
+        ForeignKeyConstraint(['RezeptID'], ['rezepte.ID'], onupdate='CASCADE', name='speiseplan_rezepte'),
+        ForeignKeyConstraint(['RezeptkategorieID'], ['rezeptkategorien.ID'], onupdate='CASCADE', name='speiseplan_rezeptkategorien'),
+        Index('Datum_RezeptkategorieID', 'Datum', 'RezeptkategorieID', unique=True),
+        Index('speiseplan_rezepte', 'RezeptID'),
+        Index('speiseplan_rezeptkategorien', 'RezeptkategorieID')
+    )
+
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    Datum: Mapped[datetime.date] = mapped_column(Date)
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+    RezeptkategorieID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    RezeptID: Mapped[Optional[int]] = mapped_column(INTEGER(11))
+    Anmerkung: Mapped[Optional[str]] = mapped_column(String(400))
+
+    rezepte: Mapped['Rezepte'] = relationship('Rezepte', back_populates='speiseplan')
+    rezeptkategorien: Mapped['Rezeptkategorien'] = relationship('Rezeptkategorien', back_populates='speiseplan')
+
+
+class ZutatenAllergene(db.Model):
+    __tablename__ = 'zutaten_allergene'
+    __table_args__ = (
+        ForeignKeyConstraint(['AllergenID'], ['allergene.ID'], ondelete='CASCADE', onupdate='CASCADE', name='zutaten_allergene_allergene'),
+        ForeignKeyConstraint(['ZutatID'], ['zutaten.ID'], ondelete='CASCADE', onupdate='CASCADE', name='zutaten_allergene_zutaten'),
+        Index('ZutatID_AllergenID', 'ZutatID', 'AllergenID', unique=True),
+        Index('zutaten_allergene_allergene', 'AllergenID')
+    )
+
+    ID: Mapped[int] = mapped_column(INTEGER(11), primary_key=True)
+    ZutatID: Mapped[int] = mapped_column(INTEGER(11))
+    AllergenID: Mapped[int] = mapped_column(INTEGER(11))
+    Stand: Mapped[datetime.datetime] = mapped_column(TIMESTAMP, server_default=text('current_timestamp() ON UPDATE current_timestamp()'))
+
+    allergene: Mapped['Allergene'] = relationship('Allergene', back_populates='zutaten_allergene')
+    zutaten: Mapped['Zutaten'] = relationship('Zutaten', back_populates='zutaten_allergene')
