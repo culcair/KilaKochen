3969a7671742fc35b77940d5443afd7d9a79e712
Christian Hasenburg <christian.hasenburg@kinderladen-jakobistrasse.de>
2024-07-15 13:13:34 +0200

    renaming und Wochenansciht aktiv

--- kilakochen/main/views.py
+++ kilakochen/main/views.py
@@ -18,22 +18,31 @@ def get_rezepte(kategorie):
     return [(x.ID, x.Titel) for x in res]
 
 @login_required
-@bp.route('/heute/edit', methods=['GET', 'POST'])
-def heute_edit():
-    date = datetime.today().date()
-    heute = Essensplan.query.filter_by(Datum=date).one_or_none()
+@bp.route('/day/edit/<string:raw_date>', methods=['GET', 'POST'])
+@bp.route('/today/edit', methods=['GET', 'POST'],defaults= {"raw_date" : datetime.today().date()})
+def day_edit(raw_date):
+    if type(raw_date) == str:
+        given_date = datetime.fromisoformat(raw_date).date()
+    else:
+        given_date = raw_date
+            
+    print(given_date)
+
+    plan = Essensplan.query.filter_by(Datum=given_date).one_or_none()
     
     form = EditHeuteForm()
     form.hauptgericht.choices = get_rezepte("H")
     form.beilage.choices = get_rezepte("B")
     form.dessert.choices = get_rezepte("D")
-    if heute is not None:
-        form.datum.data = heute.Datum
+    if plan is not None:
+        form.datum.data = plan.Datum
+    else:
+        form.datum.data = given_date
 
     if form.validate_on_submit():
-        if heute is None:
-            heute = Essensplan(
-                Datum                   = date,
+        if plan is None:
+            plan = Essensplan(
+                Datum                   = given_date,
                 HauptgerichtRezeptID    = form.hauptgericht.data,
                 BeilageRezeptID         = form.beilage.data,
                 DessertRezeptID         = form.dessert.data,
@@ -41,58 +50,64 @@ def heute_edit():
                 Anmerkung               = form.anmerkung.data
             )
         else:
-            heute.HauptgerichtRezeptID  = form.hauptgericht.data
-            heute.BeilageRezeptID       = form.beilage.data
-            heute.DessertRezeptID       = form.dessert.data
-            heute.Ausfall               = form.ausfall.data
-            heute.Anmerkung             = form.anmerkung.data
+            plan.HauptgerichtRezeptID  = form.hauptgericht.data
+            plan.BeilageRezeptID       = form.beilage.data
+            plan.DessertRezeptID       = form.dessert.data
+            plan.Ausfall               = form.ausfall.data
+            plan.Anmerkung             = form.anmerkung.data
 
-        db.session.add(heute)
+        db.session.add(plan)
         db.session.commit()
-        return redirect(url_for('main.heute'))
-    return render_template('edit_heute.html', form=form)
+        return redirect(url_for('main.day'))
+    return render_template('edit_day.html', form=form)
 
-@bp.route('/heute')
-def heute():
-    date = datetime.now()
-    data = Essensplan.query.filter_by(Datum=date.date()).one_or_none()
+@bp.route('/day/<string:raw_date>')
+@bp.route('/today')
+def day(raw_date = datetime.today().date()):
+    if type(raw_date) == str:
+        given_date = datetime.fromisoformat(raw_date).date()
+    else:
+        given_date = raw_date
+ 
+    print(raw_date)
+    data = Essensplan.query.filter_by(Datum=given_date).one_or_none()
 
     if data is None and current_user.is_authenticated:
         data = Essensplan()
-        return redirect(url_for('main.heute_edit'))
+        return redirect(url_for('main.day_edit',raw_date=given_date))
     elif data is None:
         return abort(404)
     else:
         return render_template(
-            'heute.html',
-            date = date,
+            'today.html',
+            date = given_date,
             page_title = "Essensplan von heute",
             data=data
         )
 
 
-@bp.route('/rezepte')
-def rezepte():
+@bp.route('/recipe/overview')
+def recipe_overview():
     data = Rezepte.query.order_by(Rezepte.Titel).all()
     return render_template(
-        'rezepte.html',
+        'recipe_overview.html',
         page_title = "Übersicht der Rezepte",
         data=data
     )
 
-@bp.route('/rezept/<int:id>')
-def rezept(id):
+@bp.route('/recipe/<int:id>')
+def recipe(id):
     data = Rezepte.query.filter_by(ID=id).one_or_404()
     
     return render_template(
-        'rezept.html',
+        'recipe.html',
         page_title = "Rezept - " + data.Titel,
         rezept_name = data.Titel,
         data=data
     )
 
-@bp.route('/rezept/<int:id>/print')
-def print_rezept(id):    
+@bp.route('/recipe/<int:id>/print')
+def print_recipe(id):    
     data = Rezepte.query.filter_by(ID=id).one_or_404()
     
     filename="{}_{}.pdf".format("KiLaKochen",data.ID)
@@ -106,8 +121,8 @@ def print_rezept(id):
 
     return html
 
-@bp.route('/zutaten')
-def zutaten():
+@bp.route('/ingredients')
+def ingredients():
     zutaten = Zutaten.query.all()
     allergene = Allergene.query.all()
     return render_template(
@@ -117,8 +132,8 @@ def zutaten():
         allergene = allergene
     )
 
-@bp.route('/wochenplan')
-def wochenplan():
+@bp.route('/week')
+def week():
     date = datetime.today().date()
     week = get_week(date)
     plaene = []
@@ -131,7 +146,7 @@ def wochenplan():
             print(res.Hauptgericht.Titel)
 
     return render_template(
-        'wochenplan.html',
+        'week.html',
         page_title = "Wochenplan",
         plaene = plaene
     )
@@ -145,13 +160,13 @@ def get_week(given_date,given_offset=0):
     return dates
 
 
-@bp.route('/wochenplan/edit/<string:raw_date>')
-@bp.route('/wochenplan/edit/', defaults = {'raw_date' : datetime.today()})
+@bp.route('/week/edit/<string:raw_date>')
+@bp.route('/week/edit/', defaults = {'raw_date' : datetime.today().date()})
 @login_required
-def edit_wochenplan(raw_date):
+def edit_week(raw_date):
     form = EditWocheForm()
     if type(raw_date) == str:
-        given_date = datetime.fromisoformat(raw_date)
+        given_date = datetime.fromisoformat(raw_date).date()
     else:
         given_date = raw_date
 
