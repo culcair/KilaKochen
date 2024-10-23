b3cde4f555f34786a78845f131b53961d2ae1af2
Christian Hasenburg <christian.hasenburg@kinderladen-jakobistrasse.de>
2024-10-23 14:16:08 +0200

    * Schutz gegen SQL Injection

--- kilakochen/auth/forms.py
+++ kilakochen/auth/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf          import FlaskForm
 from wtforms            import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+def validate_login(form,field):
+	message = 'Benutzername ist nicht alphanumerisch'
+	if not field.data.isalnum():
+		raise ValidationError(message)
+
 
 class LoginForm(FlaskForm):
-	username    = StringField  ('Username'  , validators=[DataRequired()])
+
+	username    = StringField  ('Username'  , validators=[DataRequired(),validate_login])
 	password    = PasswordField('Password'  , validators=[DataRequired()])
 	login       = SubmitField('Login')
