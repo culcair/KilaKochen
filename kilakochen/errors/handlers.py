f57c5ac0f7733f69b91af1289ad8519b921c33fe
Christian Hasenburg <christian.hasenburg@kinderladen-jakobistrase.de>
2026-04-27 07:52:33 +0200

    Error-Handler-Informationen erweitert.

--- kilakochen/errors/handlers.py
+++ kilakochen/errors/handlers.py
@@ -1,28 +1,28 @@
-from flask import render_template,current_app
+from flask import render_template,current_app,request
 from kilakochen import db
 from kilakochen.errors import bp
 
 
 @bp.app_errorhandler(401)
 def not_found_error(error):
-    current_app.logger.error(error)
+    current_app.logger.error(f"Fehlermeldung: {error}, Pfad: {request.url}")
     return render_template("errors/401.html"), 401
 
 
 @bp.app_errorhandler(403)
 def not_allowed(error):
-    current_app.logger.error(error)
+    current_app.logger.error(f"Fehlermeldung: {error}, Pfad: {request.url}")
     return render_template("errors/403.html"), 403
 
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
-    current_app.logger.error(error)
+    current_app.logger.error(f"Fehlermeldung: {error}, Pfad: {request.url}")
     return render_template("errors/404.html"), 404
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    current_app.logger.error(error)
+    current_app.logger.error(f"Fehlermeldung: {error}, Pfad: {request.url}")
     return render_template("errors/500.html"), 500
