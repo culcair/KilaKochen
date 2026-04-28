99c6e106c99c2907b2ded0f7b38e3f0392bc580c
Christian Hasenburg <christian.hasenburg@kinderladen-jakobistrase.de>
2026-04-28 20:39:21 +0200

    Bootstrap-Bundle lokal hinzugefügt.

--- config.py
+++ config.py
@@ -22,9 +22,6 @@ class Config:
     ADMINS = ["webmaster@kinderladen-jakobistrasse.de"]
     LANGUAGES = ["en", "de"]
     MS_TRANSLATOR_KEY = os.environ.get("MS_TRANSLATOR_KEY")
-    BOOTSTRAP_BOOTSWATCH_THEME = "minty"
-    BOOTSTRAP_SERVE_LOCAL = True
-    BOOTSTRAP_JS_FILENAME= os.environ.get("BOOTSTRAP_JS_FILENAME") or "bootstrap.min.js"
     SESSION_COOKIE_SECURE = os.environ.get("SESSION_COOKIE_SECURE") or True
     SESSION_COOKIE_SAMESITE = os.environ.get("SESSION_COOKIE_SAMESITE") or "Lax"
     SESSION_COOKIE_HTTPONLY = os.environ.get("SESSION_COOKIE_HTTPONLY") or True
