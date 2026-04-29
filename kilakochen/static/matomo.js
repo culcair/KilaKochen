95221186e6684071142e9c126628616dffe08b8d
Christian Hasenburg <christian.hasenburg@kinderladen-jakobistrasse.de>
2026-04-29 11:53:13 +0200

    Matomo nicht als inline JS

---
+++ kilakochen/static/matomo.js
@@ -0,0 +1,13 @@
+const _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+    const u = "//analytics.bapo-feinholz.de/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
+})();
